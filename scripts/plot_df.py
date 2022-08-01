9ab19382d8cedf4842a0513cf91fa6e47628428b
raquellewei <raquelle70679@gmail.com>
2022-08-01 12:53:58 -0400

    add plot_df.py

--- scripts/plot_df.py
+++ scripts/plot_df.py
@@ -11,12 +11,12 @@ def main():
     parser.add_argument('-s', '--save', type=str, help="If wants to save df for future use, file name to save as")
     parser.add_argument('-p', '--phenotype', type=str, help="Condition/site of interest.")
     parser.add_argument('-c', '--column', type=str, help="Column name in which -p input falls.")
-    parser.add_argument('-h', '--hue', type=str, help="Hue", nargs='?', default="Method")
+    parser.add_argument('-hue', '--hue', type=str, help="Hue", nargs='?', default="Method")
     parser.add_argument('-t', '--type', type=str, help="Plot type.", choices=['box', 'line'])
 
     args = parser.parse_args()
     dataframe_file = args.file
-    df = pd.read_table(dataframe_file, index_col=0)
+    df = pd.read_table(dataframe_file)
     s = pd.Series(list(df[args.column]))
 
     try:
@@ -24,14 +24,22 @@ def main():
     except:
         print("Phenotype not found in column specified.")
 
+    x = args.x
+    y = args.y
     if args.column:
         df = df[df[args.column] == args.phenotype]
-
+        print(df)
     if args.type == 'box':
-        sns.boxplot(x=args.x, y=args.y, hue=args.hue)
+        print("box plot")
+        print(df.columns)
+        sns.boxplot(x=x, y=y, hue=args.hue, data=df)
     # sns.set_theme(style="ticks", palette="pastel")
     elif args.type == 'line':
         sns.lineplot(x=args.x, y=args.y, hue=args.hue, data=df)
     # sns.lineplot(x=x, y="Silhouette", hue="method", data=df, err_style="bars", ci="sd")
-    plt.savefig(args.save)
+    #plt.savefig(args.save)
+    plt.show()
 
+
+if __name__ == "__main__":
+    main()
