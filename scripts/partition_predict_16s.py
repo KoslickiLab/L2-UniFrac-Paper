4b9cb0e2cedb4e3de869079322aebd113510cb44
raquellewei <raquelle70679@gmail.com>
2022-08-03 13:36:09 -0400

    debug

--- scripts/partition_predict_16s.py
+++ scripts/partition_predict_16s.py
@@ -139,9 +139,11 @@ def get_average_sample(sample_list, Tint, lint, nodes_in_order):
 	all_vectors = []
 	for vector in sample_list:
 		pushed_vector = L2U.push_up(vector, Tint, lint, nodes_in_order)
+		print("len of pushed vector:", len(pushed_vector))
 		all_vectors.append(pushed_vector)
 	mean_vector = L2U.mean_of_vectors(all_vectors)
 	average_sample_vector = L2U.inverse_push_up(mean_vector, Tint, lint, nodes_in_order)
+	print(len(average_sample_vector))
 	return average_sample_vector
 
 def get_label(test_sample, rep_sample_dict, Tint, lint, nodes_in_order):
