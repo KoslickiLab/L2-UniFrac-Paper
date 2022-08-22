433976d63cdd3c138dea73604365e05924fd9755
raquellewei <raquelle70679@gmail.com>
2022-08-22 19:14:36 -0400

    debug

--- scripts/partition_predict_wgs.py
+++ scripts/partition_predict_wgs.py
@@ -142,12 +142,12 @@ def try_cluster(init_n, max_try, true_n, clustering_method, clustering_basis, me
 		else:
 			print("After trying {} times it worked".format(init_n))
 	elif clustering_method.lower() == "kmeans":
-		prediction, sample_ids = get_KMeans_prediction(list(clustering_basis.keys()), clustering_basis, init_n) #clustering_basis = sample:vector dict
+		prediction, sample_ids = get_KMeans_prediction(clustering_basis, init_n) #clustering_basis = sample:vector dict
 		sample_index_dict = get_index_dict(list(clustering_basis.keys()))
 		group_label_dict = get_group_label_dict(prediction, sample_index_dict, meta_dict)
 		while len(set(group_label_dict.values())) < true_n and init_n < max_try:
 			init_n+=1
-			prediction, sample_ids = get_KMeans_prediction(list(sample_index_dict.keys()), clustering_basis,
+			prediction, sample_ids = get_KMeans_prediction(clustering_basis,
 													  init_n)  # clustering_basis = sample:vector dict
 			group_label_dict = get_group_label_dict(prediction, sample_index_dict, meta_dict)
 		if len(set(group_label_dict.values())) < true_n:
