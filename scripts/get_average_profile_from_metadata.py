6d7858fe0f76e84f04ea0c0dc633e373c3155d5c
raquellewei <raquelle70679@gmail.com>
2022-09-22 12:00:14 -0400

    bug fix

--- scripts/get_average_profile_from_metadata.py
+++ scripts/get_average_profile_from_metadata.py
@@ -15,7 +15,7 @@ def generate_rep_sample_from_metadata(meta_dict, profile_list, save_dir):
 	Tint, lint, nodes_in_order, nodes_to_index = L2U.get_wgs_tree(profile_path_lst)
 	targets = [meta_dict[i] for i in profile_list]
 	#profile list should come from meta_dict
-	pheno_sample_dict = get_pheno_sample_dict(profile_list, targets)
+	pheno_sample_dict = get_pheno_sample_dict(profile_path_lst, targets)
 	rep_sample_dict = get_rep_sample_dict(pheno_sample_dict, Tint, lint, nodes_in_order, nodes_to_index)
 	print(rep_sample_dict.keys())
 	for pheno in rep_sample_dict.keys():
