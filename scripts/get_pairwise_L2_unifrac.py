41ad2c1df90c5292caaf1d327705dbdf678a6d51
raquellewei <raquelle70679@gmail.com>
2022-10-18 14:26:18 -0400

    add files to get pairwise L2 UniFrac

---
+++ scripts/get_pairwise_L2_unifrac.py
@@ -0,0 +1,39 @@
+import argparse
+import pandas as pd
+import numpy as np
+import os
+import sys
+sys.path.append('L2-UniFrac')
+sys.path.append('L2-UniFrac/src')
+sys.path.append('L2-UniFrac/scripts')
+import argparse
+import L2UniFrac as L2U
+from parse_data import parse_otu_table, parse_tree_file
+import itertools as it
+
+
+def argument_parser():
+    parser = argparse.ArgumentParser(description="Get pairwise L2-UniFrac matrix from otu table")
+    parser.add_argument('-i', '--otu_file', type=str, required=True, help='Path to the input otu file in tsv format.')
+    parser.add_argument('-t', '--tree_file', type=str, required=True, help='Path to tree file.')
+    parser.add_argument('-od', '--output_file', type=str, help='File path to save the distance matrix file as.')
+    return parser
+
+def main():
+    parser = argument_parser()
+    args = parser.parse_args()
+    sample_vector_dict, sample_ids, otus = parse_otu_table(args.otu_file, normalize=True)
+    Tint, lint, nodes_in_order = parse_tree_file(args.tree_file)
+    dim = len(sample_ids)
+    dist_matrix = np.zeros(shape=(dim, dim))
+    for pair in it.combinations(sample_ids, 2): #all pairwise combinations
+        sample_p, sample_q = pair[0], pair[1]
+        unifrac = L2U.L2UniFrac_weighted_plain(Tint, lint, nodes_in_order, sample_p, sample_q)
+        i = sample_ids.index(pair[0])
+        j = sample_ids.index(pair[1])
+        dist_matrix[i][j] = dist_matrix[j][i] = unifrac
+    pd.DataFrame(data=dist_matrix, index=sample_ids, columns=sample_ids).to_csv(args.output_file, sep="\t")
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/parse_data.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+from dendropy import Tree, datamodel
+
+def parse_otu_table(otu_file, normalize=True):
+    '''
+    Parses an otu file in tsv format and returns a dict with keys being sample id and values being abundance vector
+    :param otu_file: path to an otu table file in .tsv format. Can be converted from .biom format by running
+    'biom convert -i table.biom -o table.from_biom.txt --to-tsv'
+    :return:
+    '''
+    df = pd.read_table(otu_file, header=1, index_col=0) #remove first row "#Constructed from biom file"
+    sample_ids = df.columns.tolist()
+    otus = df.index.tolist()
+    sample_vector_dict = dict()
+    for sample in sample_ids:
+        vector = df[sample].tolist()
+        if normalize is True:
+            vector = vector/np.sum(vector)
+        sample_vector_dict[sample] = vector
+    return sample_vector_dict, sample_ids, otus
+
+def parse_tree_file(tree_str_file, suppress_internal_node_taxa=True, suppress_leaf_node_taxa=False):
+	'''
+	Tint,lint,nodes_in_order = parse_tree_file(tree_str_file)
+	This function will parse a newick tree file (in the file given by tree_str_file) and return the dictionary of ancestors Tint.
+	Tint indexes the nodes by integers, Tint[i] = j means j is the ancestor of i.
+	lint is a dictionary returning branch lengths: lint[i,j] = w(i,j) the weight of the edge connecting i and j.
+	nodes_in_order is a list of the nodes in the input tree_str such that T[i]=j means nodes_in_order[j] is an ancestor
+	of nodes_in_order[i]. Nodes are labeled from the leaves up.
+	:param tree_str_file: A tree file in newick format
+	'''
+	dtree = Tree.get(path=tree_str_file, schema="newick",
+							suppress_internal_node_taxa=suppress_internal_node_taxa,
+							store_tree_weights=True,
+							suppress_leaf_node_taxa = suppress_leaf_node_taxa)
+	#Name all the internal nodes
+	nodes = dtree.nodes()
+	i=0
+	for node in nodes:
+		if node.taxon == None:
+			node.taxon = datamodel.taxonmodel.Taxon(label="temp"+str(i))
+			i = i+1
+	full_nodes_in_order = [item for item in dtree.levelorder_node_iter()]  # i in path from root to j only if i>j
+	full_nodes_in_order.reverse()
+	nodes_in_order = [item.taxon.label for item in full_nodes_in_order]  # i in path from root to j only if i>j
+	Tint = dict()
+	lint = dict()
+	nodes_to_index = dict(zip(nodes_in_order, range(len(nodes_in_order))))
+	for i in range(len(nodes_in_order)):
+		node = full_nodes_in_order[i]
+		parent = node.parent_node
+		if parent != None:
+			Tint[i] = nodes_to_index[parent.taxon.label]
+			if isinstance(node.edge.length, float):
+				lint[nodes_to_index[node.taxon.label], nodes_to_index[parent.taxon.label]] = node.edge.length
+			else:
+				lint[nodes_to_index[node.taxon.label], nodes_to_index[parent.taxon.label]] = 0.0
+	return Tint,lint,nodes_in_order
