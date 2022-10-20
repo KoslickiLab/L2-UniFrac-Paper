850788ad7ca35d90e18fa7104d17ec509b66290a
raquellewei <raquelle70679@gmail.com>
2022-10-20 15:28:40 -0400

    add get_representative_sample_otu_file.py

---
+++ scripts/get_representative_sample_otu_file.py
@@ -0,0 +1,48 @@
+import argparse
+import pandas as pd
+import numpy as np
+import os
+import sys
+sys.path.append('L2-UniFrac')
+sys.path.append('L2-UniFrac/src')
+sys.path.append('L2-UniFrac/scripts')
+import argparse
+import L2UniFrac as L2U
+try:
+    SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(os.path.dirname(SCRIPT_DIR))
+except:
+    pass
+from src.parse_data import parse_otu_table, parse_tree_file
+from src.helper import get_meta_samples_dict, get_metadata_dict
+
+def argument_parser():
+    parser = argparse.ArgumentParser(description="This function takes in a .tsv otu file and a metadata file,"
+                                                 "produces representative samples for each phenotype under a specified"
+                                                 "column in the metadata file, and writes the representative samples into"
+                                                 "a new .tsv otu file.")
+    parser.add_argument('-i', '--otu_file', type=str, required=True, help='Path to the input otu file in tsv format.')
+    parser.add_argument('-t', '--tree_file', type=str, required=True, help='Path to tree file.')
+    parser.add_argument('-o', '--output_file', type=str, help='File path to save the new otu file as.')
+    parser.add_argument('-m', '--meta_file', type=str, required=True, help='Path to metadata file.')
+    parser.add_argument('-k', '--key', type=str, help="Key column name in metadata file. Usually sample ids.")
+    parser.add_argument('-v', '--val', type=str, help="Value column name in metadata file. e.g. diagnosis, environment, etc.")
+
+def main():
+    parser = argument_parser()
+    args = parser.parse_args()
+    Tint, lint, nodes_in_order = parse_tree_file(args.tree_file)
+    sample_vector_dict, sample_ids = parse_otu_table(args.otu_file, nodes_in_order, normalize=True)
+    #push up all the samples
+    simple_meta_dict = get_metadata_dict(args.meta_file, val_col=args.val, key_col=args.key)
+    meta_samples_dict = get_meta_samples_dict(simple_meta_dict)
+    rep_sample_dict = L2U.get_rep_sample_dict(sample_vector_dict, meta_samples_dict, Tint, lint, nodes_in_order)
+    df = pd.DataFrame(rep_sample_dict, columns=sample_ids, index=nodes_in_order)
+    for sample in sample_ids:
+        df[sample] = sample_vector_dict[sample]
+    df.to_csv(args.output_file, sep='\t')
+    return
+
+
+if __name__ == '__main__':
+    main()
--- src/helper.py
+++ src/helper.py
@@ -5,20 +5,67 @@ sys.path.append('L2-UniFrac/src')
 sys.path.append('L2-UniFrac/scripts')
 import L2UniFrac as L2U
 import pandas as pd
+from skbio.stats.ordination import pcoa
+from skbio import DistanceMatrix #to install: pip install scikit-bio
+from sklearn.model_selection import train_test_split
+
+
+#classification
+def partition_sample(meta_dict, random_state, test_size=0.2):
+	'''
+	Partitions samples in the meta_dict into training and testing sets
+	:param meta_dict:
+	:param percentage: percentage of training data.
+	:return: train_dict, test_dict, {phenotype: [profile IDs]}
+	'''
+	sample_id = list(meta_dict.keys())
+	targets = list(meta_dict.values()) #true phenotypes
+	samples_train, samples_test, targets_train, targets_test = train_test_split(sample_id, targets, test_size=test_size, random_state=random_state)
+	return samples_train, samples_test, targets_train, targets_test
+
+def get_pcoa(dist_matrix, sample_lst, meta_file, col_name, plot_title):
+	'''
+	Get a PCOA plot based on the distance matrix, colored according to metadata
+	:param dist_matrix:
+	:param sample_lst:
+	:param meta_dict: a dict in the form of {id: {'environment': phenotype}}, can be obtained by calling make_metadata_dict_for_pcoa
+	:param plot_title:
+	:return:
+	'''
+	df = pd.read_table(meta_file)
+	print(df.head())
+	dm = DistanceMatrix(dist_matrix, sample_lst)
+	dist_pc = pcoa(dm)
+	return dist_pc.plot(df=df, column=col_name, cmap="Set1", title=plot_title, axis_labels=('PC1', 'PC2', 'PC3'))
 
 def get_metadata_dict(meta_file, val_col = "HMgDB_diagnosis", key_col = "library_id"):
 	'''
-	Parse a metadata file to obtain a dict with keys in key_col and values in val_col
-	:param meta_file: a metadata file containing columns that can be used as keys and values
-	:param val_col: column name for values
-	:param key_col: column name for keys
-	:return: a dict with keys in key_col and values in val_col, and a list of profile ids
+	Given a file containing metadata, return a dictionary with keys specified in key_col and values specified in val_col
+	Usually the key is the id and the value is the phenotype or class a sample belongs to
+	:param meta_file: Path to the file containing metadata.
+	:param val_col: Column title of the meta_file to be used as keys
+	:param key_col: Column title of the meta_file to be used as values
+	:return: A dictionary with keys being values of key_col and values being values of val_col. e.g. sample_id:phenotype
 	'''
-	meta_dict = dict()
+	simple_meta_dict = dict()
 	df = pd.read_csv(meta_file)
 	for i, id in enumerate(df[key_col]):
-		meta_dict[id] = df[val_col][i]
-	return meta_dict, list(meta_dict.keys())
+		simple_meta_dict[id] = df[val_col][i]
+	return simple_meta_dict
+
+def get_meta_samples_dict(simple_meta_dict):
+	'''
+	Reverse the simple meta_dict ({sample_id: phenotype}) and obtain {phenotype: list of samples}
+	:param simple_meta_dict: A {sample_id: phenotype} dictionary, obtained by running get_metadata_dict
+	:return: a {phenotype: list of samples} dictionary
+	'''
+	meta_samples_dict = dict()
+	for sample,phenotype in simple_meta_dict.items():
+		if phenotype not in meta_samples_dict:
+			meta_samples_dict[phenotype] = [sample]
+		else:
+			meta_samples_dict[phenotype].append(sample)
+	return meta_samples_dict
 
 def get_pheno_sample_dict(sample_paths, targets):
 	'''
@@ -38,7 +85,7 @@ def get_pheno_sample_dict(sample_paths, targets):
 def get_rep_sample_dict(pheno_sample_dict, Tint, lint, nodes_in_order, nodes_to_index):
 	'''
 	Compute the representative sample using the L2 UniFrac method for each phenotype in pheno_sample_dict,
-	and return it in a dict.
+	and return it in a dict. WGS version.
 	:param pheno_sample_dict: {phenotype : [list of samples]}
 	:param Tint: A dict showing nodes and their respective ancestor
 	:param lint: A dict showing edge length
