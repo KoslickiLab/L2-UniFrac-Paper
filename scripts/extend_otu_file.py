2bb69fe64f4d9c36f2ce96061f41df4a819a6672
raquellewei <raquelle70679@gmail.com>
2022-10-21 17:59:27 -0400

    modified description

--- scripts/extend_otu_file.py
+++ scripts/extend_otu_file.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import sys
 try:
     SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
     sys.path.append(os.path.dirname(SCRIPT_DIR))
@@ -9,10 +10,9 @@ from src.parse_data import parse_otu_table, parse_tree_file
 import pandas as pd
 
 def argument_parser():
-    parser = argparse.ArgumentParser(description="This function takes in a .tsv otu file and a metadata file,"
-                                                 "produces representative samples for each phenotype under a specified"
-                                                 "column in the metadata file, and writes the representative samples into"
-                                                 "a new .tsv otu file.")
+    parser = argparse.ArgumentParser(description="A helper script. In the case where the OTU file does not include all "
+                                                 "OTUs on the tree, extend this OTU file to produce a bigger file with "
+                                                 "all OTUs on the tree present.")
     parser.add_argument('-i', '--otu_file', type=str, required=True, help='Path to the input otu file in tsv format.')
     parser.add_argument('-t', '--tree_file', type=str, required=True, help='Path to tree file.')
     parser.add_argument('-o', '--output_file', type=str, help='File path to save the new otu file as.')
