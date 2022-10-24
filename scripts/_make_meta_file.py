fd99c386265e497b7bb127c81d7480db0077e73e
raquellewei <raquelle70679@gmail.com>
2022-10-23 22:53:11 -0400

    bug fix

--- scripts/_make_meta_file.py
+++ scripts/_make_meta_file.py
@@ -6,12 +6,12 @@ def main():
                                                  "given a pairwise distance matrix file.")
     parser.add_argument('-f', '--file', type=str, help="Pairwise distance file.")
     parser.add_argument('-o', '--output', type=str, help="Output file to write to.")
-    parser.add_argument('-v', '--env_name', type=str, help="Environment name.e.g. body site, treatment")
+    parser.add_argument('-e', '--env_name', type=str, help="Environment name.e.g. body site, treatment")
 
     args = parser.parse_args()
 
     df = pd.read_table(args.file, header=0, index_col=0)
-    meta_df = pd.DataFrame(columns=args.env_name, index=df.columns) #col names = environment name, sample name = phenotypes
+    meta_df = pd.DataFrame(columns=[args.env_name], index=df.columns) #col names = environment name, sample name = phenotypes
     meta_df[args.env_name] = df.columns
     meta_df.to_csv(args.output, sep='\t')
 
