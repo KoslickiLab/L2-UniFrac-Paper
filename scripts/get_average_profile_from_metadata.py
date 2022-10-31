238dacf7740552d838ba22b5aaa211c3c2a7480c
raquellewei <raquelle70679@gmail.com>
2022-10-31 18:38:58 -0400

    modify helper function import

--- scripts/get_average_profile_from_metadata.py
+++ scripts/get_average_profile_from_metadata.py
@@ -8,20 +8,28 @@ import argparse
 import L2UniFrac as L2U
 import numpy as np
 import pandas as pd
-from helper import get_metadata_dict, get_pheno_sample_dict, get_rep_sample_dict, write_vector_to_file
+from helper import get_metadata_dict, get_pheno_sample_dict, get_rep_sample_dict_wgs, write_vector_to_file
+
 
 def generate_rep_sample_from_metadata(meta_dict, profile_list, save_dir):
+	'''
+
+	:param meta_dict:
+	:param profile_list:
+	:param save_dir:
+	:return:
+	'''
 	profile_path_lst = [os.path.join(profile_dir, file) for file in os.listdir(profile_dir)]
 	Tint, lint, nodes_in_order, nodes_to_index = L2U.get_wgs_tree(profile_path_lst)
 	targets = [meta_dict[i] for i in profile_list]
 	#profile list should come from meta_dict
 	pheno_sample_dict = get_pheno_sample_dict(profile_path_lst, targets)
-	rep_sample_dict = get_rep_sample_dict(pheno_sample_dict, Tint, lint, nodes_in_order, nodes_to_index)
+	rep_sample_dict = get_rep_sample_dict_wgs(pheno_sample_dict, Tint, lint, nodes_in_order, nodes_to_index)
 	for pheno in rep_sample_dict.keys():
 		print(pheno)
 		file_name = save_dir + '/' + pheno + '.txt'
 		print('check vector sum to 1')
-		print(np.sum(rep_sample[pheno]))
+		print(np.sum(rep_sample_dict[pheno]))
 		write_vector_to_file(rep_sample_dict[pheno], file_name, nodes_in_order, nodes_to_index)
 	return
 
