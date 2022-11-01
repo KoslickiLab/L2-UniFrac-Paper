2731133a12b97ca022b9b18e998366ecec289198
raquellewei <raquelle70679@gmail.com>
2022-10-31 23:40:50 -0400

    modify paradigm

--- scripts/get_average_profile_from_metadata.py
+++ scripts/get_average_profile_from_metadata.py
@@ -11,7 +11,7 @@ import pandas as pd
 from helper import get_metadata_dict, get_pheno_sample_dict, get_rep_sample_dict_wgs, write_vector_to_file
 
 
-def generate_rep_sample_from_metadata(meta_dict, profile_list, save_dir):
+def generate_rep_sample_from_metadata(meta_dict, profile_dir, save_dir):
 	'''
 
 	:param meta_dict:
@@ -19,9 +19,15 @@ def generate_rep_sample_from_metadata(meta_dict, profile_list, save_dir):
 	:param save_dir:
 	:return:
 	'''
-	profile_path_lst = [os.path.join(profile_dir, file) for file in os.listdir(profile_dir)]
+	profile_list = os.listdir(profile_dir)
+	for profile_name in profile_list:
+		if not profile_name.endswith('.profile'):
+			profile_list.remove(profile_name)
+	profile_name_list = list(map(lambda x: x.strip('.')[0], profile_list))
+	print("quick check on profile list:", profile_list[:5])
+	profile_path_lst = [os.path.join(profile_dir, file) for file in profile_list]
 	Tint, lint, nodes_in_order, nodes_to_index = L2U.get_wgs_tree(profile_path_lst)
-	targets = [meta_dict[i] for i in profile_list]
+	targets = [meta_dict[i] for i in profile_name_list]
 	#profile list should come from meta_dict
 	pheno_sample_dict = get_pheno_sample_dict(profile_path_lst, targets)
 	rep_sample_dict = get_rep_sample_dict_wgs(pheno_sample_dict, Tint, lint, nodes_in_order, nodes_to_index)
@@ -49,6 +55,6 @@ if __name__ == '__main__':
 	id_col = args.id_col
 	save_dir = args.save
 
-	meta_dict, profile_list = get_metadata_dict(metadata_file, val_col=metadata_key, key_col=id_col)
-	generate_rep_sample_from_metadata(meta_dict, profile_list, save_dir)
+	meta_dict = get_metadata_dict(metadata_file, val_col=metadata_key, key_col=id_col)
+	generate_rep_sample_from_metadata(meta_dict, profile_dir, save_dir)
 
