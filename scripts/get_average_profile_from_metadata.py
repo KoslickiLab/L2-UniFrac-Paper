8916a126b6a3fbeaec76c495685e641f85a9a39a
raquellewei <raquelle70679@gmail.com>
2022-12-20 17:24:02 -0500

    added --out_format option to give choices of otu or cami format output

--- scripts/get_average_profile_from_metadata.py
+++ scripts/get_average_profile_from_metadata.py
@@ -11,7 +11,7 @@ import pandas as pd
 from helper import get_metadata_dict, get_pheno_sample_dict, get_rep_sample_dict_wgs, write_rep_samples_to_file
 
 
-def generate_rep_sample_from_metadata(meta_dict, profile_dir, outfile_name):
+def generate_rep_sample_from_metadata(meta_dict, profile_dir, outfile_name, out_format='cami'):
 	'''
 
 	:param meta_dict:
@@ -26,12 +26,17 @@ def generate_rep_sample_from_metadata(meta_dict, profile_dir, outfile_name):
 	profile_name_list = list(map(lambda x: x.split('.')[0], profile_list))
 	profile_path_lst = [os.path.join(profile_dir, file) for file in profile_list]
 	Tint, lint, nodes_in_order, nodes_to_index = L2U.get_wgs_tree(profile_path_lst)
-	print(meta_dict)
 	targets = [meta_dict[i] for i in profile_name_list]
 	#profile list should come from meta_dict
 	pheno_sample_dict = get_pheno_sample_dict(profile_path_lst, targets)
 	rep_sample_dict = get_rep_sample_dict_wgs(pheno_sample_dict, Tint, lint, nodes_in_order, nodes_to_index)
-	write_rep_samples_to_file(rep_sample_dict, outfile_name, nodes_in_order, nodes_to_index)
+	if out_format == 'otu':
+		write_rep_samples_to_file(rep_sample_dict, outfile_name, nodes_in_order, nodes_to_index)
+	else:
+		rep_profiles_dict = L2U.build_profiles_from_dict(rep_sample_dict, nodes_in_order)
+		for id, profile in rep_profiles_dict.items():
+			out_file_name = out_file_name.split('.')[0] + '_' + str(id) + '.profile'
+			profile.write_file(out_file_name)
 	return
 
 
@@ -40,8 +45,11 @@ if __name__ == '__main__':
 	parser.add_argument('-m', '--meta_file', type=str, help='A metadata file.', nargs='?')
 	parser.add_argument('-id_col', '--id_col', type=str, help='Name of the id column in the metadata file.', nargs='?', default="library_id")
 	parser.add_argument('-s', '--save', type=str, help="Save output file as.")
+	parser.add_argument('-o', '--out_dir', type=str, help="Save output files under this directory.")
 	parser.add_argument('-d', '--pdir', type=str, help="Directory of profiles")
 	parser.add_argument('-p', '--phenotype', type=str, help='A selected phenotype corresponding to a column name in the metadata file.', nargs='?', default="HMgDB_diagnosis")
+	parser.add_argument('-f','--out_format', type=str, help='The format of output files. Choices: cami, otu. If otu is chosen, '
+														'-s flag is required. Otherwise, -o is required.', choices=['cami', 'otu'], nargs='?', default='cami')
 
 	args = parser.parse_args()
 	metadata_file = args.meta_file
@@ -51,5 +59,6 @@ if __name__ == '__main__':
 	outfile_name = args.save
 
 	meta_dict = get_metadata_dict(metadata_file, val_col=metadata_key, key_col=id_col)
-	generate_rep_sample_from_metadata(meta_dict, profile_dir, outfile_name)
+	rep_sample_dict = generate_rep_sample_from_metadata(meta_dict, profile_dir, outfile_name, args.out_format)
+
 
