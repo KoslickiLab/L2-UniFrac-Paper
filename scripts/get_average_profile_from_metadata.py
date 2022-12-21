8fc087d57d41b34fef48d7a537a1ab9578dfc37f
raquellewei <raquelle70679@gmail.com>
2022-12-21 16:37:11 -0500

    added leaves_only option

--- scripts/get_average_profile_from_metadata.py
+++ scripts/get_average_profile_from_metadata.py
@@ -11,7 +11,7 @@ import pandas as pd
 from helper import get_metadata_dict, get_pheno_sample_dict, get_rep_sample_dict_wgs, write_rep_samples_to_file
 
 
-def generate_rep_sample_from_metadata(meta_dict, profile_dir, outfile_name, out_format='cami'):
+def generate_rep_sample_from_metadata(meta_dict, profile_dir, outfile_name, out_format='cami', leaves_only=False):
 	'''
 
 	:param meta_dict:
@@ -34,9 +34,8 @@ def generate_rep_sample_from_metadata(meta_dict, profile_dir, outfile_name, out_
 		write_rep_samples_to_file(rep_sample_dict, outfile_name, nodes_in_order, nodes_to_index)
 	else:
 		index_to_nodes = {v: k for k, v in nodes_to_index.items()}
-		rep_profiles_dict = L2U.build_profiles_from_dict(rep_sample_dict, nodes_in_order, index_to_nodes)
+		rep_profiles_dict = L2U.build_profiles_from_dict(rep_sample_dict, nodes_in_order, index_to_nodes, leaves_only)
 		for id, profile in rep_profiles_dict.items():
-			profile._add_up()
 			out_file_name = outfile_name.split('.')[0] + '_' + str(id) + '.profile'
 			profile.write_CAMI_file(out_file_name)
 	return
@@ -48,6 +47,7 @@ if __name__ == '__main__':
 	parser.add_argument('-id_col', '--id_col', type=str, help='Name of the id column in the metadata file.', nargs='?', default="library_id")
 	parser.add_argument('-s', '--save', type=str, help="Save output file as.")
 	parser.add_argument('-d', '--pdir', type=str, help="Directory of profiles")
+	parser.add_argument('-l', '--leaves_only', type=str, help="Only have abundances on leaf notes?", nargs='?', default='n', choices=['y','n'])
 	parser.add_argument('-p', '--phenotype', type=str, help='A selected phenotype corresponding to a column name in the metadata file.', nargs='?', default="HMgDB_diagnosis")
 	parser.add_argument('-f', '--out_format', type=str, help='The format of output files. Choices: cami, otu. If otu is chosen, '
 														'-s flag is required. Otherwise, -o is required.', choices=['cami', 'otu'], nargs='?', default='cami')
@@ -60,6 +60,6 @@ if __name__ == '__main__':
 	outfile_name = args.save
 
 	meta_dict = get_metadata_dict(metadata_file, val_col=metadata_key, key_col=id_col)
-	rep_sample_dict = generate_rep_sample_from_metadata(meta_dict, profile_dir, outfile_name, args.out_format)
+	rep_sample_dict = generate_rep_sample_from_metadata(meta_dict, profile_dir, outfile_name, args.out_format, args.leaves_only)
 
 
