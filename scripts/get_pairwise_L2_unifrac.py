dc116e12cd930fe5b278bd0abbcc0a308696a3d8
raquellewei <raquelle70679@gmail.com>
2023-03-21 16:29:21 -0400

    added L1 functionality to get_pairwise_L1_unifrac.py

--- scripts/get_pairwise_L2_unifrac.py
+++ scripts/get_pairwise_L2_unifrac.py
@@ -4,8 +4,10 @@ import sys
 sys.path.append('L2-UniFrac')
 sys.path.append('L2-UniFrac/src')
 sys.path.append('L2-UniFrac/scripts')
+sys.path.append('src')
 import argparse
 import L2UniFrac as L2U
+import L1UniFrac as L1U
 try:
     SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
     sys.path.append(os.path.dirname(SCRIPT_DIR))
@@ -16,13 +18,14 @@ from extract_data import parse_tree_file
 import pandas as pd
 
 def argument_parser():
-    parser = argparse.ArgumentParser(description="Get pairwise L2-UniFrac matrix from otu table")
+    parser = argparse.ArgumentParser(description="Get pairwise L1 or L2-UniFrac matrix from otu table")
     parser.add_argument('-i', '--otu_file', type=str, required=True, help='Path to the input otu file in tsv format. The'
                                                                           'number of OTUs in the table must be the same'
                                                                           'as that of the tree file. If not, first run'
                                                                           'extend_otu_file.py')
-    parser.add_argument('-t', '--tree_file', type=str, required=True, help='Path to tree file.')
+    parser.add_argument('-t', '--tree_file', type=str, required=True, help='Path to tree file.', default='data/trees/gg_13_5_otus_99_annotated.tree')
     parser.add_argument('-o', '--output_file', type=str, help='File path to save the distance matrix file as.')
+    parser.add_argument('-L1', '--L1', type=int, default=0, help='pairwise L1 UniFrac instead of L2.')
     return parser
 
 def main():
@@ -36,13 +39,17 @@ def main():
     sample_vector_dict = df.to_dict(orient='list')
     dim = len(sample_ids)
     dist_matrix = np.zeros(shape=(dim, dim))
-    for pair in it.combinations(sample_ids, 2): #all pairwise combinations
-        sample_p, sample_q = sample_vector_dict[pair[0]], sample_vector_dict[pair[1]]
-        i = sample_ids.index(pair[0])
-        j = sample_ids.index(pair[1])
-        unifrac = L2U.L2UniFrac_weighted_plain(Tint, lint, nodes_in_order, sample_p, sample_q)
-        dist_matrix[i][j] = dist_matrix[j][i] = unifrac
-    pd.DataFrame(data=dist_matrix, index=sample_ids, columns=sample_ids).to_csv(args.output_file, sep="\t")
+    if args.L1 == 1:
+        df = L1U.pairwise_L1EMDUniFrac_weighted(sample_vector_dict, Tint, lint, nodes_in_order)
+        df.to_csv(args.output_file, sep='\t')
+    else:
+        for pair in it.combinations(sample_ids, 2): #all pairwise combinations
+            sample_p, sample_q = sample_vector_dict[pair[0]], sample_vector_dict[pair[1]]
+            i = sample_ids.index(pair[0])
+            j = sample_ids.index(pair[1])
+            unifrac = L2U.L2UniFrac_weighted_plain(Tint, lint, nodes_in_order, sample_p, sample_q)
+            dist_matrix[i][j] = dist_matrix[j][i] = unifrac
+        pd.DataFrame(data=dist_matrix, index=sample_ids, columns=sample_ids).to_csv(args.output_file, sep="\t")
 
 
 if __name__ == '__main__':
