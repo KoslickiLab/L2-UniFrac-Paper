1ea66ccd6ef1ad65db55521ab95f696585a47da5
raquellewei <raquelle70679@gmail.com>
2023-03-27 14:17:12 -0400

    fix typo

--- scripts/get_representative_sample_otu_file.py
+++ scripts/get_representative_sample_otu_file.py
@@ -52,7 +52,7 @@ def main():
     df = pd.DataFrame.from_dict(rep_sample_dict)
     df.index = nodes_in_order
     df.to_csv(args.output_file, sep='\t')
-    condensed_df = df.drop([i for i in df.index if i.startswih('temp')])
+    condensed_df = df.drop([i for i in df.index if i.startswith('temp')])
     print(condensed_df)
     if args.L1_average:
         L1_rep_sample_dict = L1U.get_L1_representative_sample_16s(sample_vector_dict, meta_samples_dict, Tint, lint,
